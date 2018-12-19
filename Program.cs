0e719ba1f1b3961c8c81b0348bd739a14ef471d6
nuttanon <nuttanon001@hotmail.com>
2018-12-19 13:40:31 +0700

    config program.cs

--- Program.cs
+++ Program.cs
@@ -19,6 +19,8 @@ namespace Template_Program
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                .UseIISIntegration()
                 .UseStartup<Startup>();
     }
 }
